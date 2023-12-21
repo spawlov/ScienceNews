7b27ffd727cbd5d22a757741f9d2841c106f5865
sergey <spawlov@gmail.com>
2023-12-21 22:20:13 +0700

    Added models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, verbose_name="url")
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True, verbose_name="url")
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, verbose_name="url")
+    author = models.CharField(max_length=100)
+    content = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="опубликовано")
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    views = models.IntegerField(default=0, verbose_name="количество просмотров")
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name="posts")
+    tags = models.ManyToManyField(Tag, blank=True, related_name="posts")
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.title

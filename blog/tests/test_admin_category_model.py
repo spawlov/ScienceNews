c5f38c936017b53ccef8a384623d8988a8033285
sergey <spawlov@gmail.com>
2024-01-17 06:08:03 +0700

    Refactoring

--- blog/tests/test_admin_category_model.py
+++ blog/tests/test_admin_category_model.py
@@ -1,32 +1,34 @@
 from django.contrib.admin.options import ModelAdmin
 from django.contrib.auth.models import User
-from django.test import TestCase
 from django.contrib.admin.sites import AdminSite
 from django.http import HttpRequest
+from django.test import TestCase
+
 from ..models import Category
 from ..admin import CategoryAdmin
 
 
 class CategoryAdminTest(TestCase):
-
     def setUp(self):
         self.site = AdminSite()
-        self.user = User.objects.create_superuser('admin', 'admin@example.com', 'password')
-        self.client.login(username='admin', password='password')
+        self.user = User.objects.create_superuser(
+            "admin", "admin@example.com", "password"
+        )
+        self.client.login(username="admin", password="password")
 
     def test_category_model(self):
         model_admin = ModelAdmin(Category, self.site)
-        self.assertEqual(str(model_admin), 'blog.ModelAdmin')
+        self.assertEqual(str(model_admin), "blog.ModelAdmin")
 
     def test_save_model(self):
         request = HttpRequest()
-        request.method = 'POST'
+        request.method = "POST"
         request.user = self.user
-        category = Category(title='Test Category')
+        category = Category(title="Test Category")
         admin = CategoryAdmin(Category, self.site)
         form_class = admin.get_form(request)
-        form = form_class(data={'title': 'Test Category'}, instance=category)
+        form = form_class(data={"title": "Test Category"}, instance=category)
         change = True
         admin.save_model(request, category, form, change)
-        self.assertEqual(category.slug, 'test-category')
-        self.assertEqual(category.title, 'Test Category')
+        self.assertEqual(category.slug, "test-category")
+        self.assertEqual(category.title, "Test Category")

e4981f468f41daf3474cce25be59f689798108e5
sergey <spawlov@gmail.com>
2024-01-17 05:55:37 +0700

    Refactoring

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-
 from .views import Home, PostDetail, PostByCategory, PostByTag
 
 urlpatterns = [
-    path('', Home.as_view(), name='home'),
-    path('post/<str:slug>/', PostDetail.as_view(), name='post'),
-    path('category/<str:slug>/', PostByCategory.as_view(), name='category'),
-    path('tag/<str:slug>/', PostByTag.as_view(), name='tag'),
+    path("", Home.as_view(), name="home"),
+    path("post/<str:slug>/", PostDetail.as_view(), name="post"),
+    path("category/<str:slug>/", PostByCategory.as_view(), name="category"),
+    path("tag/<str:slug>/", PostByTag.as_view(), name="tag"),
 ]

72c406031999460dbc8e6d356a155e985f63a8ed
sergey <spawlov@gmail.com>
2024-01-17 06:10:30 +0700

    Refactoring

--- blog/tests/test_admin_post_model.py
+++ blog/tests/test_admin_post_model.py
@@ -1,13 +1,14 @@
 import os
 import sys
 import tempfile
-from unittest import skipIf
 
 from django.contrib.admin.options import ModelAdmin
 from django.contrib.admin.sites import AdminSite
 from django.contrib.auth.models import User
 from django.http import HttpRequest
 from django.test import TestCase
+
+from unittest import skipIf
 from lorem import get_paragraph
 
 from ..admin import PostAdmin
@@ -15,90 +16,102 @@ from ..models import Post, Category
 
 
 class CategoryAdminTest(TestCase):
-
     def setUp(self):
         self.site = AdminSite()
-        self.user = User.objects.create_superuser('admin', 'admin@example.com', 'password')
-        self.client.login(username='admin', password='password')
-        self.category = Category.objects.create(title='Test Category', slug='test-category')
-        self.content = get_paragraph(5, sep='\n', comma=(0, 2), word_range=(4, 8), sentence_range=(5, 10))
-        self.photo = tempfile.NamedTemporaryFile(suffix='.jpg').name
+        self.user = User.objects.create_superuser(
+            "admin", "admin@example.com", "password"
+        )
+        self.client.login(username="admin", password="password")
+        self.category = Category.objects.create(
+            title="Test Category", slug="test-category"
+        )
+        self.content = get_paragraph(
+            count=5,
+            sep="\n",
+            comma=(0, 2),
+            word_range=(4, 8),
+            sentence_range=(5, 10),
+        )
+        self.photo = tempfile.NamedTemporaryFile(suffix=".jpg").name
 
     def test_category_model(self):
         model_admin = ModelAdmin(Post, self.site)
-        self.assertEqual(str(model_admin), 'blog.ModelAdmin')
+        self.assertEqual(str(model_admin), "blog.ModelAdmin")
 
-    @skipIf(sys.platform.startswith('win'), 'requires Linux')
+    @skipIf(sys.platform.startswith("win"), "requires Linux")
     def test_upload_photo(self):
         post = Post.objects.create(
-            title='Test Post',
+            title="Test Post",
             content=self.content,
             category=self.category,
             photo=self.photo,
         )
-        self.assertEqual(post.photo.url, f'/media{self.photo}')
+        self.assertEqual(post.photo.url, f"/media{self.photo}")
 
-    @skipIf(sys.platform.startswith('win'), 'requires Linux')
+    @skipIf(sys.platform.startswith("win"), "requires Linux")
     def test_get_photo_with_image(self):
         post = Post.objects.create(
-            title='Test Post',
+            title="Test Post",
             content=self.content,
             category=self.category,
             photo=os.path.normpath(self.photo),
         )
         admin = PostAdmin(Post, self.site)
         self.assertEqual(
-            str(admin.get_photo(post)), f'<img src="/media{self.photo}" style="max-height: 200px;">'
+            str(admin.get_photo(post)),
+            f'<img src="/media{self.photo}" style="max-height: 200px;">',
         )
 
     def test_get_photo_without_image(self):
         post = Post.objects.create(
-            title='Test Post',
+            title="Test Post",
             content=self.content,
             category=self.category,
             photo=None,
         )
         admin = PostAdmin(Post, self.site)
-        self.assertEqual(str(admin.get_photo(post)), '-')
+        self.assertEqual(str(admin.get_photo(post)), "-")
 
-    @skipIf(sys.platform.startswith('win'), 'requires Linux')
+    @skipIf(sys.platform.startswith("win"), "requires Linux")
     def test_get_list_photo_with_image(self):
         post = Post.objects.create(
-            title='Test Post',
+            title="Test Post",
             content=self.content,
             category=self.category,
             photo=os.path.normpath(self.photo),
         )
         admin = PostAdmin(Post, self.site)
         self.assertEqual(
-            str(admin.get_list_photo(post)), f'<img src="/media{self.photo}" style="max-height: 50px;">'
+            str(admin.get_list_photo(post)),
+            f'<img src="/media{self.photo}" style="max-height: 50px;">',
         )
 
     def test_get_list_photo_without_image(self):
         post = Post.objects.create(
-            title='Test Post',
+            title="Test Post",
             content=self.content,
             category=self.category,
             photo=None,
         )
         admin = PostAdmin(Post, self.site)
-        self.assertEqual(str(admin.get_list_photo(post)), '-')
+        self.assertEqual(str(admin.get_list_photo(post)), "-")
 
     def test_save_model(self):
         request = HttpRequest()
-        request.method = 'POST'
+        request.method = "POST"
         request.user = self.user
-        post = Post(title='Test Post')
+        post = Post(title="Test Post")
         admin = PostAdmin(Post, self.site)
         form_class = admin.get_form(request)
         form = form_class(
             data={
-                'title': 'Test Post',
-                'category': self.category,
-                'content': self.content,
+                "title": "Test Post",
+                "category": self.category,
+                "content": self.content,
             },
-            instance=post)
+            instance=post,
+        )
         change = True
         admin.save_model(request, post, form, change)
-        self.assertEqual(post.slug, 'test-post')
-        self.assertEqual(post.title, 'Test Post')
+        self.assertEqual(post.slug, "test-post")
+        self.assertEqual(post.title, "Test Post")

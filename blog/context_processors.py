fa2ad5bb1f4001d27445dc3601553481ad706a3e
sergey <spawlov@gmail.com>
2024-01-17 10:52:00 +0700

    Added popular posts function

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,11 +1,10 @@
 from blog.models import Category, Post
 
 
-def get_categories_for_navigate(request):
+def get_general_context(request):
     menu_category = Category.objects.all()
-    return {"menu_category": menu_category}
-
-
-def get_five_popular_posts(request):
     popular_posts = Post.objects.all().order_by("-views")[:5]
-    return {"five_popular_posts": popular_posts}
+    return {
+        "menu_category": menu_category,
+        "five_popular_posts": popular_posts,
+    }
--- siteblog/settings.py
+++ siteblog/settings.py
@@ -74,7 +74,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "blog.context_processors.get_categories",
+                "blog.context_processors.get_general_context",
             ],
         },
     },

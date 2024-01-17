15ed3628d048bc5b88d8b64e9f747489527e0e20
sergey <spawlov@gmail.com>
2024-01-17 10:35:51 +0700

    Division categories and tags pages

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -3,4 +3,4 @@ from blog.models import Category
 
 def get_categories(request):
     menu_category = Category.objects.all()
-    return {"menu_category ": menu_category}
+    return {"menu_category": menu_category}

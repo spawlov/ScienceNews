4cb7007105f777f3f34079ab8611634323b37963
sergey <spawlov@gmail.com>
2024-01-17 10:46:15 +0700

    Added popular posts function

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,6 +1,11 @@
-from blog.models import Category
+from blog.models import Category, Post
 
 
-def get_categories(request):
+def get_categories_for_navigate(request):
     menu_category = Category.objects.all()
     return {"menu_category": menu_category}
+
+
+def get_five_popular_posts(request):
+    popular_posts = Post.objects.all().order_by("-views")[:5]
+    return {"five_popular_posts": popular_posts}

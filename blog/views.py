c9d554b48beb109561f98d1806cb48e9f0419467
sergey <spawlov@gmail.com>
2024-01-19 17:27:29 +0700

    Updated titles

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ class Home(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["title"] = "Classic Blog Design"
+        context["title"] = "Home"
         return context
 
 

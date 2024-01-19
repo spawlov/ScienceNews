c9e4b047473329c002db4307f8fff7a2879a2e67
sergey <spawlov@gmail.com>
2024-01-19 14:40:43 +0700

    Added tags limit for tags cloud

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -4,7 +4,7 @@ from .models import Category, Post, Tag
 def get_general_context(request):
     menu_category = Category.objects.all()
     posts = Post.objects.all()
-    tags = Tag.objects.all()
+    tags = Tag.objects.all().order_by("?")[:50]
     popular_posts = posts.order_by("-views")[:5]
     random_post = posts.order_by("?").first()
     return {

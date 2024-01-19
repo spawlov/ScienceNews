b7e0895107bf370aefd99e45c968f782ad6ca16f
sergey <spawlov@gmail.com>
2024-01-19 21:10:54 +0700

    Added variable to settings.py

--- siteblog/settings.py
+++ siteblog/settings.py
@@ -22,6 +22,8 @@ DEBUG = env.bool("DEBUG", False)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", ["127.0.0.1", "localhost"])
 
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", [])
+
 INTERNAL_IPS = ["127.0.0.1", "localhost"]
 
 

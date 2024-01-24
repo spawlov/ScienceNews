60712661bf177ae7407341defb951d797e437241
sergey <spawlov@gmail.com>
2024-01-25 03:55:22 +0700

    Fixed errors

--- siteblog/settings.py
+++ siteblog/settings.py
@@ -545,18 +545,10 @@ CELERY_RESULT_SERIALIZER = "json"
 # Cache with Redis on product
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
-        "LOCATION": ".django_cache",
-    }
-}
-
-if not DEBUG:
-    CACHES = {
-        "default": {
-            "BACKEND": "django.core.cache.backends.redis.RedisCache",
-            "LOCATION": "redis://127.0.0.1:6379",
-            "OPTIONS": {
-                "db": 1,
-            },
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+        "OPTIONS": {
+            "db": 1,
         },
-    }
+    },
+}

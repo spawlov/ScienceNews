d984ba5ff852ab401dca77a4bdc445dea720c851
sergey <spawlov@gmail.com>
2024-01-25 21:56:33 +0700

    Added Celery task - weekly mailing

--- blog/tasks.py
+++ blog/tasks.py
@@ -1,8 +1,41 @@
+from datetime import timedelta
+
 from celery import shared_task
 
-from django.core.management import execute_from_command_line
+from django.conf import settings
+from django.core import mail
+from django.template.loader import render_to_string
+from django.utils import timezone
+
+from .models import Post, Subscriber
 
 
 @shared_task
-def parsing():
-    execute_from_command_line(["manage.py", "parsing_naked_science"])
+def test_mailing():
+    site = settings.ALLOWED_HOSTS[0]
+    last_week = timezone.now() - timedelta(days=7)
+    posts = Post.objects.filter(
+        created_at__gte=last_week,
+        published=True,
+    ).order_by("created_at")
+    subscribers = list(
+        Subscriber.objects.all().values_list("email", flat=True),
+    )
+    for subscriber in subscribers:
+        with mail.get_connection() as connection:
+            html_content = render_to_string(
+                template_name="blog/email_templates/week_email.html",
+                context={
+                    "site": site,
+                    "posts": posts,
+                    "email": subscriber,
+                },
+            )
+            message = mail.EmailMultiAlternatives(
+                subject="Test message from Science News",
+                from_email=settings.EMAIL,
+                to=[subscriber],
+                connection=connection,
+            )
+            message.attach_alternative(html_content, "text/html")
+            message.send()
--- siteblog/celery.py
+++ siteblog/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "siteblog.settings")
 
@@ -8,3 +9,14 @@ app = Celery("siteblog")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    "parsing-for-add-news": {
+        "task": "blog.tasks.test_mailing",
+        "schedule": crontab(
+            hour="0",
+            minute="0",
+            day_of_week="monday",
+        ),
+    }
+}

034dac9f84b43bb457873bc1ae664255cc6255af
sergey <spawlov@gmail.com>
2024-01-25 12:24:19 +0700

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+echo
+echo "Starting deploy..."
+echo "=================================================================="
+echo "Pull from repository..."
+git pull
+echo "Pull from repository completed."
+echo "=================================================================="
+echo "Activating venv..."
+source .sciencenews/bin/activate
+echo "Venv activated."
+echo "=================================================================="
+echo "Updating requirements..."
+pip install -r requirements.txt
+echo "Requirements updated."
+echo "=================================================================="
+echo "Collecting staticfiles..."
+python manage.py collectstatic --noinput
+echo "Staticfiles collected."
+echo "=================================================================="
+echo "Migrations are applied..."
+python manage.py migrate --noinput
+echo "Migrations completed."
+echo "=================================================================="
+echo "Restarting Gunicorn..."
+systemctl restart sciencenews.service
+echo "Restart Gunicorn completed."
+echo "=================================================================="
+echo "Reloading Nginx..."
+systemctl reload nginx
+echo "Nginx reloaded"
+echo "=================================================================="
+chmod g+x deploy.sh
+echo "Deploy completed."

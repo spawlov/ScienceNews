0f18d3c0099c811492df86cd4d254f7f6b3c6b70
sergey <spawlov@gmail.com>
2024-03-12 21:38:56 +0700

    Updated views.py

--- blog/views.py
+++ blog/views.py
@@ -122,6 +122,9 @@ class PostsByTagView(ListView):
             )
             .order_by("-views")[:10]
         )
+        context["popular_categories"] = (
+            context["popular_posts_by_tag"].categories.all().distinct()
+        )
         tags = []
         for post in context["popular_posts_by_tag"]:
             for tag in post.tag.all():
